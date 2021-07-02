e42a5d521f24b599394bd24e193ed1236208eed1
Jeroen Claassens <support@favware.tech>
2021-07-02 20:50:38 +0200

    style: cleanup prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,14 +3,5 @@ const sapphirePrettierConfig = require('@sapphire/prettier-config');
 module.exports = {
   ...sapphirePrettierConfig,
   useTabs: false,
-  tabWidth: 2,
-  overrides: [
-    ...sapphirePrettierConfig.overrides,
-    {
-      files: 'src/assets/*.ts,json',
-      options: {
-        printWidth: 200
-      }
-    }
-  ]
+  tabWidth: 2
 };
